746ceffe854b2409d9140d0d8e0868ca47db8259
ks00545 <ks00545@srcs.org>
2020-12-18 18:39:13 -0800

    Started MovePlayers()

--- scripts/dodgeball.js
+++ scripts/dodgeball.js
@@ -4,6 +4,10 @@ DodgeballApp = {
 
     init: function () {
        
+        window.onkeydown = function(event){
+            DodgeballApp.keyPress(event);
+        }
+
     },
     createPlayer: function() {
     let player1div = document.createElement("div")
@@ -17,7 +21,7 @@ DodgeballApp = {
         element: player1div,
         x_pos: 40,
         y_pos: 250
-    }
+        }
     let player2div = document.createElement("div")
     player2div.className = "player2"
     this.container.append(player2div)
@@ -29,17 +33,35 @@ DodgeballApp = {
         element: player2div,
         x_pos: 960,
         y_pos: 250
-    }
+        }
 
     },
+
+    movePlayers: function(event) {
+        if(event.keyCode == 87){
+            DodgeballApp.Player1.x_pos = DodgeballApp.Player1.x_pos + 1;
+            console.log("ok");
+            }
+          /*else if(event.keyCode == 65){
+            SketchApp.color = 'red';
+          }
+          else if(event.keyCode == 83){
+            SketchApp.color = 'green';
+          }
+          else if(event.keyCode == 68){
+            SketchApp.color = 'blue';
+          }*/
+    },
+
     createBall: function() {
     let Ball = {
         radius = 15,
         color: white,
         x_velocity = 0,
         y_velocity = 0
-    }
+        }
     },
+
     checkForHit: function() {
 
     },
